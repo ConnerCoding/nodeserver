9a3461ebfa02ddadc999ede3cd44c7d75f696e05
Conner Schiller <conner.schiller@gmail.com>
2020-06-29 21:30:53 -0700

    adding init files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('getting root');
+});
+
+app.listen(port);
